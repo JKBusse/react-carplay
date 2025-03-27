4520a745eaea77ca5e9adef35a26ae581a05a1fb
JKBusse <48227459+JKBusse@users.noreply.github.com>
2025-03-27 23:13:21 +0100

    comm

--- src/renderer/src/worker/carplay.ts
+++ src/renderer/src/worker/carplay.ts
@@ -13,8 +13,8 @@ let config: Partial<DongleConfig> | null = null
 
 const handleMessage = (message: CarplayMessage) => {
   const { type, message: payload } = message
-  //console.log(message)
-  console.log("Message type: ", type, " Message: ", message)
+  console.log(message)
+  //console.log("Message type: ", type, " Message: ", message)
   if (type === 'video') {
     postMessage(message, [payload.data.buffer])
   } else if (type === 'audio' && payload.data) {
